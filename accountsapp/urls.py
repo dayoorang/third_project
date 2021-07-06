23c62973734c50fc3d2b84d113ac0a43d9de0c7c
yangsunkang <dayoorang@gmail.com>
2021-07-06 11:16:13 +0900

    210702 third_project practice

--- accountsapp/urls.py
+++ accountsapp/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
 from .views import (
-     index,
      hello_world
 )
 
 app_name = 'accountsapp'
 
 urlpatterns = [
-    path('', index, name = 'index'),
-    path('hello_world/', hello_world, name = 'helloworld')
+    path('hello_world/',hello_world, name ='hello_world')
 ]
--- accountsapp/views.py
+++ accountsapp/views.py
@@ -4,10 +4,5 @@ from django.shortcuts import render
 # Create your views here.
 
 
-
-def index(request):
-    return HttpResponse('this is main page')
-
-
 def hello_world(request):
-    return HttpResponse('hello world page')
+    return render(request, 'accountsapp/hello_world.html')
