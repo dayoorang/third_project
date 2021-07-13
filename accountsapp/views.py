c7dd9b3148fb3c518ed481b2be8b61859deec916
yangsunkang <dayoorang@gmail.com>
2021-07-13 11:47:51 +0900

    210713 third_project commit 1

--- accountsapp/views.py
+++ accountsapp/views.py
@@ -1,8 +1,28 @@
-from django.http import HttpResponse
+
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
+from accountsapp.models import NewModel
+
+
 
 
 def hello_world(request):
-    return render(request, 'accountsapp/hello_world.html')
+    if request.method == "POST":
+        temp = request.POST.get('input_text')
+
+        new_model = NewModel()
+        new_model.text = temp
+        new_model.save()
+
+        data_list = NewModel.objects.all()
+
+        return HttpResponseRedirect(reverse('accountsapp:hello_world'))
+    else:
+        data_list = NewModel.objects.all()
+
+        return render(request, 'accountsapp/hello_world.html',
+                      context={'data_list': data_list})
