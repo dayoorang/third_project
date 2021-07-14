90d9b7dec7162cd621b215a611885d774081f798
yangsunkang <dayoorang@gmail.com>
2021-07-14 18:12:48 +0900

    210714 third_project commit 2

--- accountsapp/urls.py
+++ accountsapp/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from .views import (
     hello_world, AccountCreateView
@@ -7,6 +8,10 @@ app_name = 'accountsapp'
 
 urlpatterns = [
     path('hello_world/',hello_world, name ='hello_world'),
-    path('create/', AccountCreateView.as_view(), name='create')
+    path('create/', AccountCreateView.as_view(), name='create'),
+    path('login/', LoginView.as_view(template_name='accountsapp/login.html'),
+         name='login'),
+    path('logout/', LogoutView.as_view(template_name='accountsapp/logout.html'),
+         name='logout')
 
 ]
