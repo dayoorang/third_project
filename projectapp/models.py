114cb58645a6a15322b9cf8e10894a8bf2f161ea
yangsunkang <dayoorang@gmail.com>
2021-08-17 12:18:41 +0900

    8/17 third_project commit 1

--- projectapp/models.py
+++ projectapp/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class Project(models.Model):
     name = models.CharField(max_length=20, null=False)
     description = models.CharField(max_length=200, null=True)
-    image = models.ImageField(upload_to='project/', null=False)
+    image = models.ImageField(upload_to='project/', null=True, blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
