f5e776808eeeeda826211e8384bdad42914f1787
yangsunkang <dayoorang@gmail.com>
2021-08-20 17:16:04 +0900

    8/20 third_project commit 2

--- subscribeapp/urls.py
+++ subscribeapp/urls.py
@@ -1,7 +1,10 @@
 
 from django.urls import path, include
 
+from subscribeapp.views import SubscriptionView
+
+app_name = 'subscribeapp'
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('subscribe/<int:project_pk>', SubscriptionView.as_view(), name='subscribe'),
 
 ]
